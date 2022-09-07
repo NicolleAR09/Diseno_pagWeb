b59c0fe6739c496c8f670cf1603e46accd8c821f
Nicolle <antequeran@uninorte.edu.co>
2022-09-07 10:42:51 -0500

    Code with the map

---
+++ map.js
@@ -0,0 +1,30 @@
+    const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+
+    let myMap = L.map('myMap'). setView([10.9886091, -74.7922088], 13)
+
+    L.tileLayer(tilesProvider,{
+        maxZoom: 18,
+        attribution: '© OpenStreetMap'
+    }).addTo(myMap)
+
+    let marker = L.marker([10.9886091, -74.7922088]).addTo(myMap)
+
+    var circle = L.circle([10.9886091, -74.7822088], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+    }).addTo(myMap);
+
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+    var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(myMap);
+}
+
+map.on('click', onMapClick);
