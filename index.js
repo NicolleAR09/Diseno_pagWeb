91a77d1e37a74e59915d20b1c0c0abc35abd7848
Nicolle <antequeran@uninorte.edu.co>
2022-09-14 12:31:58 -0500

    Adding html to index.js

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ app.get('/', (req, res) => res.send('Hello World from Express!'))
 
 app.listen(8000);
 console.log('Server on port 8000')
+
+//Llamado HTML
+app.get('/',(req,res)=>{    
+    res.sendFile(__dirname+'/index2.html')
+})
