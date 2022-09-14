7d1e40ce0d6d681e5260a8d0ac4961f10009c224
LE0NARD0R <laalvear@uninorte.edu.co>
2022-09-14 15:55:09 -0500

    Avances organizados

--- static/map.js
+++ static/map.js
@@ -9,9 +9,20 @@
 
     let marker = L.marker([10.9886091, -74.7922088]).addTo(myMap)
 
-    marker.bindPopup("<b>Latitud: 10.9886091 Longitud: -74.7922088</b>").openPopup();
+    marker.bindPopup("<b>lat:10.9886091, long:-74.7922088 </b>").openPopup();
 
     const popup = L.popup();
+
+    const latlngs = [
+        [10.9896091, -74.7922088],
+        [10.9906091, -74.7922088],
+        [10.9916091, -74.7922088]
+    ];
+    
+    const polyline = L.polyline(latlngs, {color: 'red'}).addTo(myMap);
+    
+    // zoom the map to the polyline
+    myMap.fitBounds(polyline.getBounds());
 
 function onMapClick(e) {
     popup
