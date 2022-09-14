93acd639bc1a9b2536fa5b236d379dad5f314f96
Nicolle <antequeran@uninorte.edu.co>
2022-09-14 12:43:17 -0500

    Correction index.js

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const express = require('express');
 
 app.get('/', (req, res) => res.send('Hello World from Express!'))
 
-app.listen(8000);
-console.log('Server on port 8000')
-
 //Llamado HTML
 app.get('/',(req,res)=>{    
     res.sendFile(__dirname+'/index2.html')
 })
+
+app.listen(8000);
+console.log('Server on port 8000')
+
