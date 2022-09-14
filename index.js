6713bfa8401f4fa201c9cb056d3886c8299850bd
Nicolle <antequeran@uninorte.edu.co>
2022-09-14 14:28:59 -0500

    el +

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
 
 app.listen(8000, ()=>console.log('Mi servidor está corriendo sobre el puerto 8000'))
 // Contenido estático
-//app.use(express.static(__dirname+'/index2.html'));
+app.use(express.static(__dirname+'/index2.html'));
 
 app.get('/', (req, res) =>{    
     res.sendFile(__dirname+'/index2.html')
