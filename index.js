36453328cae4ec0115617d8fde5b5ca6a9d70b5a
Nicolle <antequeran@uninorte.edu.co>
2022-09-14 12:20:47 -0500

    new Index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');    
+
+ const app = express();
+
+app.get('/', (req, res) => res.send('Hello World from Express!'))
+
+app.listen(8000);
+console.log('Server on port 8000')
