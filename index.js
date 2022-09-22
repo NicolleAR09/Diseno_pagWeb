f94e2cb41dd85a1b36f00e5652605ea88eba40cc
LE0NARD0R <laalvear@uninorte.edu.co>
2022-09-22 10:08:24 -0500

    historic leo changes in the main

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');    
 require('dotenv').config();
 
- const app = express();
+const app = express();
 
-//Addition of the database
+//-----------------------connection with database
+
 const mysql = require('mysql')
 
 DBConfig = {
@@ -23,18 +24,20 @@ connection.connect((error)=>{
   
 })
 
- const data = {
+//---------------------------------------------------
+
+const data = {
     Latitud: 0,
     Longitud: 0,
     Date: "00/00/00",
-    Time: "00:00:00"
- };
-
- app.get("/data", (req, res) => {
+    Time: "00:00:00" 
+  };
+app.get("/data", (req, res) => {
     const json = JSON.stringify(data)
     return res.status(200).send(json);
-});
+  });
 
+//--------------------------------get info from the message
 const dgram = require('dgram');
 const socket = dgram.createSocket('udp4');
 
@@ -42,7 +45,6 @@ socket.on('error', (err) => {
   console.log(`server error:\n${err.stack}`);
   socket.close();
 });
-
 socket.on('message', async (msg, senderInfo) => {
   console.log('Messages received ' + msg)
   const infoMensaje = String(msg).split(",")
@@ -56,7 +58,7 @@ socket.on('listening', (req, res) => {
   console.log(`UDP server listening on: ${address.address}:${address.port}`);
 });
 
-// insert data to the satabase
+//-------------------------------------------insert info to database
 const insertData = (info) => {
     data.Latitud = info[0];
     data.Longitud = info[1];
@@ -70,50 +72,9 @@ const insertData = (info) => {
     console.log("Received: ", data);
 };
 
-//initializing server
+//-----------------------------------------initializing server
 app.use(express.static(__dirname+'/static'));
 app.listen(8000, ()=>console.log('Mi servidor está corriendo sobre el puerto 8000'));
+
+//----------------------------------------port from sniffer
 socket.bind(8050);
-
-
-
-// Contenido estático
-
- /*
-app.get('/', (req, res) =>{    
-    res.sendFile(__dirname+'/index2.html')
-})
-
-
-
-//----------------------------------------------------------------
-
-const insertData = async (info) => {
-  const lat = info[0];
-  const long = info[1];
-  const date = info[2];
-  const hour = info[3];
-  const query = `INSERT INTO gpsdata (lat, long, date, hour) VALUES (${lat}, ${long}, "${date}","${hour}")`;
-  connection.query(query, function(err, result){
-    if(err)throw err;
-    console.log("Registro guardado exitosamente.")
-  })
-};
-
-//Llamado HTML
-app.get('/index2',(req,res)=>{    
-    res.sendFile(__dirname+'/index2.html')
-})
-
-app.get("/data", (req, res) => {
-    const json = JSON.stringify(data)
-    return res.status(200).send(json);
-})
-
-//app.listen(8000);
-//console.log('Server on port 8000')
-
-app.listen(8000, ()=>console.log('Mi servidor está corriendo sobre el puerto 8000'))
-// Contenido estático
-app.use(express.static(__dirname+'/index2.html'));
-*/
