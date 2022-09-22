a9752d6e9013b87b72046fbb1ebcf17f68c8ed2d
JavierOrozco01 <jearroyo@uninorte.edu.co>
2022-09-22 10:31:42 -0500

    no hay modificaciones

--- static/map.js
+++ static/map.js
@@ -6,6 +6,6 @@ L.tileLayer(tilesProvider,{
     maxZoom: 18,
     attribution: '© OpenStreetMap'
 }).addTo(myMap)
-
+// Inicializacion del marker y la linea que dibuja la polylinea
 let marker = L.marker([10.9886091, -74.7922088]).addTo(myMap)
 const line = L.polyline([], {color: 'red'}).addTo(myMap);
