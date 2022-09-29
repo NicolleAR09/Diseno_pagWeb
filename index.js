836a0ff52fbfa8b3077fd589577da93bb992f068
samgj18 <samgj_18@hotmail.com>
2022-09-29 19:05:23 +0100

    Unreferencing socket

--- index.js
+++ index.js
@@ -139,5 +139,4 @@ app.listen(8000, () =>
 );
 
 //----------------------------------------port from sniffer
-socket.bind(8050);
-console.log("Socker listening on port 8050");
+socket.bind(8050).unref();
