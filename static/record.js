3fd767a95e7f71fdfb1846f4e9c48cb21e1da285
LE0NARD0R <laalvear@uninorte.edu.co>
2022-10-24 22:38:49 -0500

    trying to fix monitors error

--- static/record.js
+++ static/record.js
@@ -116,6 +116,10 @@ const showRecordInfo = async () => {
     //Historic 2
     myMap.on("onclick", (e) => {
         console.log(histPolyline);
+        
+        setLatLng([e.latlng.lat, e.latlng.lng]).addTo(myMap);
+
+        L.marker([e.latlng.lat, e.latlng.lng]).addTo(myMap);
 
         var marker = null;
 
@@ -126,9 +130,7 @@ const showRecordInfo = async () => {
         marker = L.marker(e.latlng).addTo(map);
         });
         
-        setLatLng([e.latlng.lat, e.latlng.lng]).addTo(myMap);
-
-        L.marker([e.latlng.lat, e.latlng.lng]).addTo(myMap);
+        
 
         fetch(`/pathg?latd=${e.latlng.lat}&longd=${e.latlng.lng}`, {
             method: "GET",
