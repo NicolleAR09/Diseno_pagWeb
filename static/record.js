42e9854c02103035f01f2c71fa311c764c52d022
LE0NARD0R <laalvear@uninorte.edu.co>
2022-11-05 12:26:06 -0500

    cambiando el buscar el punto más cercano

--- static/record.js
+++ static/record.js
@@ -240,8 +240,8 @@ const showRecordInfo = async () => {
                         var i;
                         let points1 = [];
                         for(i=0;i<n;i++ ){
-                            console.log(points[i]);
                             //points1[i] = new Date (Date.parse(points[i])).toString;
+                            console.log((Date.parse(points[i])).toString);
                         }
                         //console.log(points1);
                         
